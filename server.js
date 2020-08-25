76658593598d8ffd259724744d2c48789783f2a5
unknown <corwin_hope@yahoo.com>
2020-08-25 11:42:21 -0400

    M

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 var cors = require("cors");
-app.use(cors());
+//app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 
 
 
@@ -129,6 +138,7 @@ app.post("/score", authenticateToken, async function (req, res) {
     });
   }
 });
+
 
 
 function authenticateToken(req, res, next) {
